e84a06ed0964689633cb8d9bf8e6e31ff4e234b5
Mattia-Cocco <io.mattick@gmail.com>
2021-03-01 17:33:25 +0100

    Update cut_frame.py

--- proof_of_concept/modulo_1_get_frame/cut_frame.py
+++ proof_of_concept/modulo_1_get_frame/cut_frame.py
@@ -1,5 +1,12 @@
 #1)sudo apt-get install python3-pip
 #2)python3 -m pip install image_slicer
+import glob, os
 import image_slicer
-frame=image_slicer.slice('Frame/frame8.jpg', 6, save=False)
-image_slicer.save_tiles(frame, directory='FrameCut/')
+
+print("hello")
+count = 0
+for file in glob.glob("Frame/*.jpg"):
+    print("hello")
+    frames=image_slicer.slice(file, 6, save=False)
+    image_slicer.save_tiles(frames, directory='FrameCut/', prefix='slice'+str(count))
+    count += 1
