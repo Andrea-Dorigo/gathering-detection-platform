21260c84af47823fe5cc8de31c0550caa0aac8cc
Mattia-Cocco <io.mattick@gmail.com>
2021-03-01 17:15:15 +0100

    Fix cut_frame.py

--- proof_of_concept/modulo_1_get_frame/cut_frame.py
+++ proof_of_concept/modulo_1_get_frame/cut_frame.py
@@ -1,5 +1,5 @@
-#altezza/2
-#lunghezza/3
+#1)sudo apt-get install python3-pip
+#2)python3 -m pip install image_slicer
 import image_slicer
 frame=image_slicer.slice('Frame/frame8.jpg', 6, save=False)
 image_slicer.save_tiles(frame, directory='FrameCut/')
