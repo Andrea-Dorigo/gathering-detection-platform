794aeea823f62a4f9b70581a177a3c852b88e909
Mattia Cocco <mattiacocco@pop-os.localdomain>
2021-03-01 15:28:27 +0100

    Update get_frames and get_video

--- proof_of_concept/modulo_1_get_frame/get_video.py
+++ proof_of_concept/modulo_1_get_frame/get_video.py
@@ -2,11 +2,10 @@ import urllib.request
 import os
 import time
 import string
-#import get_frames
 import glob
 
 count = 1
-list_link = ("https://hddn00.skylinewebcams.com/live.m3u8?a=e43aev49lh66bfkseapbqtds90", "https://hddn00.skylinewebcams.com/live.m3u8?a=e43aev49lh66bfkseapbqtds90")
+list_link = ("https://hddn00.skylinewebcams.com/live.m3u8?a=4gsvt3jt5k84e831s2t0snjfo0", "https://cdn-002.whatsupcams.com/hls/it_roma02.m3u8")
 path_video = "FileMU/"
 
 files = [os.path.join(path_video, file) for file in os.listdir(path_video) if os.path.isfile(os.path.join(path_video, file))]
@@ -14,25 +13,28 @@ files = [os.path.join(path_video, file) for file in os.listdir(path_video) if os
 #infinite url request every 5 minutes
 
 #count perché così posso capire se stanno funzionando i link diversi, col "while true" mi tocca bloccare l'esecuzione del programma
-while count < 2:
-
+while True:
     #download file.m3u8
     for obj in range(len(list_link)):
         urllib.request.urlretrieve(list_link[obj], "FileMU/fontanaTrevi"+str(obj)+".m3u8")
-    
+        print(list_link[obj])
+
     i = 0
     list_video = []
 
-    #open & read file.m3u8  
+    #open & read file.m3u8
     for ffile in files:
-        with open(ffile) as fopen:
+        with open(ffile, "r") as fopen:
             for line in fopen.readlines():
                 clean = line[:-1]
                 if clean.endswith('.ts'):
+                    if not clean.startswith('https'):
+                        clean = 'https://cdn-002.whatsupcams.com/hls/' + clean
+                        list_video.append(clean)
                     list_video.append(clean)
 
     print(list_video)
-    
+
 
     #download video.ts i=5,7,9,11,13 are the videos location inside lines[]
     for o in range(len(list_video)):
@@ -41,5 +43,4 @@ while count < 2:
     #extract frame
     exec(open('get_frames.py').read())
 
-    #time.sleep(5*60)
-    count += 1
+    time.sleep(5*60)
