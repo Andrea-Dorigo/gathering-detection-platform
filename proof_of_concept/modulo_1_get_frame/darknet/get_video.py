6205d9a7665b0d442ce5e8a3af08f25d040a39ab
Mattia-Cocco <io.mattick@gmail.com>
2021-03-04 16:18:33 +0100

    Update get_video.py

--- proof_of_concept/modulo_1_get_frame/darknet/get_video.py
+++ proof_of_concept/modulo_1_get_frame/darknet/get_video.py
@@ -7,13 +7,15 @@ import string
 import glob
 import subprocess
 
-bashCommand = "./darknet detect cfg/yolov3.cfg yolov3.weights ../FrameCut/slice5_01_02.png"
+bashCommand = "./darknet detect cfg/yolov3.cfg yolov3.weights ../FrameCut/"
+path_frame = "../FrameCut/*.png"
 count = 1
 d = 0
 #list_link = ("https://cdn-002.whatsupcams.com/hls/it_roma02.m3u8", "https://hddn00.skylinewebcams.com/live.m3u8?a=4gsvt3jt5k84e831s2t0snjfo0")
 path_video = "../FileMU/"
 list_link = "https://cdn-002.whatsupcams.com/hls/it_roma02.m3u8"
 urllib.request.urlretrieve(list_link, "../FileMU/PiazzaNavona.m3u8")
+
 
 files = [os.path.join(path_video, file) for file in os.listdir(path_video) if os.path.isfile(os.path.join(path_video, file))]
 
@@ -43,19 +45,26 @@ while True:
 
 
     #download video.ts i=5,7,9,11,13 are the videos location inside lines[]
-    for o in range(len(list_video)):
-        urllib.request.urlretrieve(list_video[o], "../VideoFontane/Video"+str(o)+".ts")
+    #for o in range(len(list_video)):
+    #    urllib.request.urlretrieve(list_video[o], "../VideoFontane/Video"+str(o)+".ts")
+
+    urllib.request.urlretrieve(list_video[0], "../VideoFontane/Video"+str(0)+".ts")
 
     #extract frame
     exec(open('get_frames.py').read())
     #cut each frames in 6
     exec(open('cut_frame.py').read())
     #call darknet
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    process.wait()
-    output, error = process.communicate()
-    print(output.decode().count('person'))
+    conta_persone = 0
 
+    for file in glob.glob(path_frame):
+        bashCommand = "./darknet detect cfg/yolov3.cfg yolov3.weights ../FrameCut/"+ str(file)
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        process.wait()
+        output, error = process.communicate()
+        conta_persone += output.decode().count('person')
+        print(conta_persone)
 
+    print(conta_persone)
 
-    #time.sleep(5*60)
+    time.sleep(10*60)
