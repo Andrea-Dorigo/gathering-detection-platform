d95a388f034edcd85ec7c9b08af5cfb4194bee16
Andrea-Dorigo <failosc@gmail.com>
2021-03-10 20:25:04 +0100

    Add executable start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,13 @@
+sudo docker pull mongo
+
+sudo docker network create jawadruids-net
+
+sudo docker run -d --name mdb --net jawadruids-net -p 27017:27017 -e "discovery.type=single-node" mongo
+
 sudo docker build -t gdp-be modulo_1_get_frame
+
+sudo docker run -d --net jawadruids-net -p 1111:1111 --name gdp-be gdp-be
+
+sudo docker build -t gdp-fe modulo_5_web_app
+
+sudo docker run -d --net jawadruids-net -p 8080:8080 --name gdp-fe gdp-fe
