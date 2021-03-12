5522a41b23650280a2837066a8e0fcf0551b7a54
Andrea-Dorigo <failosc@gmail.com>
2021-03-12 18:31:19 +0100

    Pass pylint test  10/10 cut_frame.py

--- proof_of_concept/modulo_1_get_frame/yolo-object-detection/cut_frame.py
+++ proof_of_concept/modulo_1_get_frame/yolo-object-detection/cut_frame.py
@@ -1,11 +1,20 @@
 #1)sudo apt-get install python3-pip
 #2)python3 -m pip install image_slicer
-import glob, os
+"""
+This file is used in get_video.py
+"""
+
+import glob
 import image_slicer
 
-count = 0
-nframe = 0
-for foto in glob.glob("../Frame/*.jpg"):
-    frames=image_slicer.slice(foto, 6, save=False)
-    image_slicer.save_tiles(frames, directory='../FrameCut/', prefix='slice'+str(count))
-    count += 1
+def main():
+    """
+    This function cuts a frame in 6
+    """
+    count = 0
+    # nframe = 0
+    for foto in glob.glob("../Frame/*.jpg"):
+        frames = image_slicer.slice(foto, 6, save=False)
+        image_slicer.save_tiles(frames, directory='../FrameCut/', prefix='slice'+str(count))
+        count += 1
+main()
