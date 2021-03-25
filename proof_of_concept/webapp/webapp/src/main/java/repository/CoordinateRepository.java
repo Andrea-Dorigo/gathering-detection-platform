bbb6c7154178d007328341574fa2bb794dd905f4
Andrea Cecchin <crashctr92@gmail.com>
2021-03-25 17:40:04 +0100

    Deleted useless file

--- proof_of_concept/webapp/webapp/src/main/java/repository/CoordinateRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import model.detection;
-
-public interface CoordinateRepository extends MongoRepository<detection, String>{
-	public detection findFirstById(String id);
-}
