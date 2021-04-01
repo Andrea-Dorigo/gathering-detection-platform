e1cc70fd77a4c9255349879bef1ac5dd7c0df0d8
Andrea-Dorigo <failosc@gmail.com>
2021-04-01 10:35:04 +0200

    Define forecast main(), loop every 2 days

--- proof_of_concept/acquisition/main/weather_forecast.py
+++ proof_of_concept/acquisition/main/weather_forecast.py
@@ -1,14 +1,18 @@
-
-# Python program to find current
-# weather details of any city
-# using openweathermap api
+"""
+Python program to get weather foreacast every 30 minutes
+for all cities in webcams.
+Uses OpenWeatherMap API:
+more information at -> https://openweathermap.org/api
+"""
 
 # import required modules
 import requests, json
-from datetime import datetime
+import time
+from datetime import date, datetime, timedelta
 import pymongo
 
 # MongoDB parameters
+# MongoEngine would be more appropriate here
 CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
 DATABASE = CLIENT["GDP-test"]
 COLLECTION = DATABASE["weather_forecast"]
@@ -19,7 +23,16 @@ API_KEY = "550617cb3af649e1d6729a3f78b24e17"
 # Base OpenWeatherMap api url
 BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?"
 
+# Webcams json path
+PATH_WEBCAM_JSON = "../webcams.json"
+
+
 def get_hourly_forecast(latitude,longitude):
+    """
+    Gets hourly forecast for any given coordinates
+    for the next 48 hours.
+    Forecast variables: temperature, weather_description
+    """
 
     # url to request
     complete_url = ( BASE_URL +
@@ -53,5 +66,47 @@ def get_hourly_forecast(latitude,longitude):
                                 "temperature" : temperature })
 
 
+def main():
+    """
+    Get forecast data every 30 minutes
+    starting from the next midnight;
 
-get_hourly_forecast(41.899139,12.473311)
+    get the weather data at minutes 00/30 of every hour,
+    since that is required by the current Machine Learning model
+    """
+
+    # load json with webcams data
+    with open(PATH_WEBCAM_JSON) as f:
+      json_data = json.load(f)
+
+    # wait until midnight
+    tomorrow = datetime.today() + timedelta(days=1)
+    midnight = datetime.combine(tomorrow, datetime.min.time())
+    print( "waiting until: " + str(midnight) +
+           ";\ntime remaining: " + str(midnight - datetime.now()) )
+
+    time.sleep((midnight - datetime.now()).total_seconds())
+
+    # infinite loop
+    while False:
+
+        # get next day midnight
+        day_after_tomorrow = datetime.today() + timedelta(days=2)
+        tomorrow_midnight = datetime.combine(day_after_tomorrow, datetime.min.time())
+
+        # loop all webcams
+        for webcam in json_data["webcams"]:
+            get_hourly_forecast(webcam["latitude"], webcam["longitude"])
+
+        # sleep 30 minutes (1800 seconds)
+        time.sleep(1800)
+
+        # repeat (could be a repeat twice loop)
+        for webcam in json_data["webcams"]:
+            get_hourly_forecast(webcam["latitude"], webcam["longitude"])
+
+        # sleep until the next midnight
+        time.sleep((tomorrow_midnight - datetime.now()).total_seconds())
+
+
+main()
