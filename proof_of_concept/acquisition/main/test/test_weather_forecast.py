d519d749854d5739b9ab1e3bf1bcc830708c68a8
Andrea-Dorigo <failosc@gmail.com>
2021-04-12 16:13:03 +0200

    Reduce assertions in test_weather_forecast

--- proof_of_concept/acquisition/main/test/test_weather_forecast.py
+++ proof_of_concept/acquisition/main/test/test_weather_forecast.py
@@ -8,11 +8,7 @@ class ResponseTestCase(unittest.TestCase):
 
    def test_response(self):
        response = get_openweather_api_response(45.309812,18.41042,"550617cb3af649e1d6729a3f78b24e17")
-       # keyword "hourly" must be in the response
-       self.assertIn('hourly', response)
-       # keyword "cod" must not be in the response: it indicades the error code
-       self.assertNotIn('cod', response)
-       # results must be exactly 48 (hours)
+       # results must contain 48 hourly_forecast)
        self.assertEqual(len(response['hourly']) , 48)
 
 unittest.main()
