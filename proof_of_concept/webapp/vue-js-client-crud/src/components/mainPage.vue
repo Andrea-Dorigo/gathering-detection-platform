d5ded0e708fcb560b52009eb21c0a03cefb96cea
Emma Roveroni <emma.roveroni@studenti.unipd.it>
2021-05-20 11:46:51 +0200

    Fix csv and add query + test

--- proof_of_concept/webapp/vue-js-client-crud/src/components/mainPage.vue
+++ proof_of_concept/webapp/vue-js-client-crud/src/components/mainPage.vue
@@ -86,7 +86,6 @@ export default {
     listCity,
   },
   methods: {
-    //Quando si preme ReloadMap
     getRetrieveCoordinate: function() {
       this.$root.$refs.datePicker_component.resetDate();
       this.$root.$refs.slider_component.reloadMap();
@@ -101,23 +100,22 @@ export default {
         "Dati della città di " +
         this.$root.$refs.autocompleteSearch_component.getNameCity();
       text[1] = "";
-      Elements.getDataRT(
+      Elements.getAllValue(
         this.$root.$refs.autocompleteSearch_component.getNameCity(),
-        this.$root.$refs.datePicker_component.getDate()
       ).then((res) => {
         if (res.data != 0) {
-          console.log(res.data);
-          text[2] = "Id webcam: " + res.data[0].id_webcam;
-          console.log(res.data[0].id_webcam);
-          text[3] = "Latitudine: " + res.data[0].latitude;
-          text[4] = "Longitudine: " + res.data[0].longitude;
-          text[5] = "Numero di persone: " + res.data[0].numPeople;
-          text[6] = "Data e ora: " + res.data[0].date;
-          if (res.data[0].type == 0) text[7] = "Tipologia di dati: Reali";
+          var i;
+         for (i = 0; i < res.data.length; i++)  {
+          text[3] = "Latitudine: " + res.data[i].latitude;
+          text[4] = "Longitudine: " + res.data[i].longitude;
+          text[5] = "Numero di persone: " + res.data[i].numPeople;
+          text[6] = "Data e ora: " + res.data[i].date;
+          if (res.data[i].type == 0) text[7] = "Tipologia di dati: Reali";
           else text[7] = "Tipologia di dati: Predetti";
-          text[8] = "Meteo: " + res.data[0].weather_description;
-          text[9] = "Temperatura: " + res.data[0].temperature;
-          text[10] = "Giorno della settimana: " + res.data[0].day_of_week;
+          text[8] = "Meteo: " + res.data[i].weather_description;
+          text[9] = "Temperatura: " + res.data[i].temperature;
+          text[10] = "Giorno della settimana: " + res.data[i].day_of_week;
+          }
           doc.text(text, 10, 10);
           doc.save("dati.pdf");
         } else {
@@ -127,42 +125,45 @@ export default {
     },
     exportcsv: function() {
       var modal = document.getElementById("myModalPDF");
-      Elements.getDataRT(
+
+      Elements.getAllValue(
         this.$root.$refs.autocompleteSearch_component.getNameCity(),
-        this.$root.$refs.datePicker_component.getDate()
       ).then((res) => {
+        console.log(res.data[0])
         if (res.data != 0) {
+          var i;
+          var dati;
           var csv =
-            "ID webcam, Latitudine, Longitudine, Numero di persone, Data e ora, Tipologia di dati, Meteo, Temperatura, Giorno della settimana\n";
-          var tipo;
-          if (res.data[0].type == 0) tipo = "Tipologia di dati: Reali";
-          else tipo = "Tipologia di dati: Predetti";
-          var dati =
-            res.data[0].id_webcam +
-            "," +
-            res.data[0].latitude +
-            "," +
-            res.data[0].longitude +
-            "," +
-            res.data[0].numPeople +
-            "," +
-            res.data[0].date +
-            "," +
-            tipo +
-            "," +
-            res.data[0].weather_description +
-            "," +
-            res.data[0].temperature +
-            "," +
-            res.data[0].day_of_week +
-            "\n";
-          csv = csv + dati;
-          const anchor = document.createElement("a");
-          anchor.href =
-            "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
-          anchor.target = "_blank";
-          anchor.download = "dati.csv";
-          anchor.click();
+              "Latitudine, Longitudine, Numero di persone, Data e ora, Tipologia di dati, Meteo, Temperatura, Giorno della settimana\n";
+          for (i = 0; i < res.data.length; i++) {
+            var tipo;
+            if (res.data[0].type == 0) tipo = "Tipologia di dati: Reali";
+            else tipo = "Tipologia di dati: Predetti";
+            dati =
+              res.data[i].latitude +
+              "," +
+              res.data[i].longitude +
+              "," +
+              res.data[i].numPeople + 
+              "," +
+              res.data[i].date +
+              "," +
+              tipo +
+              "," +
+              res.data[i].weather_description +
+              "," +
+              res.data[i].temperature +
+              "," +
+              res.data[i].day_of_week +
+              "\n";
+            csv = csv + dati;
+          }
+            const anchor = document.createElement("a");
+            anchor.href =
+              "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
+            anchor.target = "_blank";
+            anchor.download = "dati.csv";
+            anchor.click();
         } else {
           modal.style.display = "block";
         }
