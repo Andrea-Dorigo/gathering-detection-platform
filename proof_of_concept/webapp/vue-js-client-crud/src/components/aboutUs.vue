cb8ed8ba6b793e408c4845ba62c800fbaf3f2114
Igli <Mezini>
2021-05-21 12:38:37 +0200

    Fixed CSS "Chi siamo"

--- proof_of_concept/webapp/vue-js-client-crud/src/components/aboutUs.vue
+++ proof_of_concept/webapp/vue-js-client-crud/src/components/aboutUs.vue
@@ -9,7 +9,7 @@
 
 <template>
   <div id="chiSiamo">
-    <h1>Il gruppo</h1>
+    <h2>Il gruppo</h2>
     <p>
       Il gruppo Jawa Druids è composto da sette studenti dell'università di
       studio di Padova del corso di laurea triennale di Informatica.
@@ -36,12 +36,12 @@
     </p>
     <ul id="loghi">
       <li id="synclab">
-        <img src="../assets/synclab.png" alt="Sync Lab" width="400" />
+        <img src="../assets/synclab.png" alt="Sync Lab"  />
       </li>
       <li>
-        <img src="../assets/DRUIDSLOGO.jpg" alt="Jawa Druids" width="400" />
+        <img src="../assets/DRUIDSLOGO.jpg" alt="Jawa Druids"  />
       </li>
-      <li><img src="../assets/unipd.png" alt="Unipd" width="400" /></li>
+      <li class="unipd"><img src="../assets/unipd.png" alt="Unipd"  /></li>
     </ul>
   </div>
 </template>
@@ -61,6 +61,7 @@ export default {
 #chiSiamo ul {
   display: flex;
   list-style-type: none;
+  
 }
 #chiSiamo #loghi li {
   padding-left: 3%;
@@ -70,4 +71,22 @@ export default {
   padding-top: 9%;
   padding-bottom: 5%;
 }
+
+img{
+max-width: 200px;
+max-height: 200px;
+width:100%;
+height: 100%;
+margin: auto;
+}
+
+.unipd{
+  padding:1em;
+}
+
+#loghi{
+  text-align: center;
+}
+
+
 </style>
